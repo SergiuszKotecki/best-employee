280e52ab61bd31ae85fe9111c2c1abd64a3fe469
skotecki <sergiusz.kotecki@itsystems.biz.pl>
2017-12-14 19:53:20 +0100

    initial commit

---
+++ api/src/main/java/com/employee/api/ResTfulApiApplication.java
@@ -0,0 +1,12 @@
+package com.employee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiApplication.class, args);
+	}
+}
