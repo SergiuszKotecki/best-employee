1ee03b91a1020cee909a8d23e3a429d72ba7eefe
skotecki <sergiusz.kotecki@itsystems.biz.pl>
2018-02-03 13:37:22 +0100

    handling jpa exceptions

--- api/src/main/java/com/employee/api/service/impl/CandidateServiceImpl.java
+++ api/src/main/java/com/employee/api/service/impl/CandidateServiceImpl.java
@@ -3,12 +3,15 @@ package com.employee.api.service.impl;
 import com.employee.api.model.Candidate;
 import com.employee.api.repository.CandidateRepository;
 import com.employee.api.service.CandidateService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.PersistenceException;
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 public class CandidateServiceImpl implements CandidateService {
@@ -18,7 +21,12 @@ public class CandidateServiceImpl implements CandidateService {
 
     @Override
     public Candidate createNew(Candidate candidate) {
-        return candidateRepository.save(candidate);
+        try {
+            return candidateRepository.save(candidate);
+        } catch (PersistenceException e) {
+            log.info("Error while saving" + candidate.toString() + " " + e.toString());
+            return candidate;
+        }
     }
 
     @Override
@@ -30,8 +38,12 @@ public class CandidateServiceImpl implements CandidateService {
     public Candidate uppdateCandidateData(String uuid, Candidate candidate) {
         Candidate cand = candidateRepository.findOne(uuid);
         cand.setFirstName(candidate.getFirstName());
-        candidateRepository.save(cand);
-        return cand;
+        try {
+            return candidateRepository.save(cand);
+        } catch (PersistenceException e) {
+            log.info("Error while saving" + cand.toString() + " " + e.toString());
+            return candidate;
+        }
     }
 
     @Override
@@ -43,7 +55,6 @@ public class CandidateServiceImpl implements CandidateService {
     public List<Candidate> getAllCandidates() {
         return candidateRepository.findAll();
     }
-
 
 
 }
